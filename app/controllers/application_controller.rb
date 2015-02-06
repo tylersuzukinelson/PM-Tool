1ea0f0aa8620f8d756aa163274cb5624101bea94
me <kotakinabalu1@hotmail.com>
2015-02-05 20:47:53 -0800

    added redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_devise_params, if: :devise_controller?
 
+  # def after_sign_up_path_for(resource)
+  #   new_project_path(current_user)
+  # end
+
 private
 	def configure_devise_params
 		devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
